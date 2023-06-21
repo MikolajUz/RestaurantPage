d938481bd24839e4f122eac1000651c93e61b878
MikeTheStrike <muzialko@gmail.com>
2023-06-21 14:02:58 +0200

    img rep

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,6 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
